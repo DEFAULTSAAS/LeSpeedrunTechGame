d82484ba7b5ec0f689cad432b53815bdbed77a79
defaultsaas <thecarldun@gmail.com>
2026-05-28 21:39:45 +0100

    Initial scripts created

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform Target;
+    public Vector2 InputAxisFactors;
+    public bool EnableFreeFly;
+    public bool FollowTarget;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerColAnimManager PCAM;
+    private Vector3 playerMoveDir;
+    private Vector3 playerLookDir;
+    private Vector3 playerInputDir;
+    private Vector3 desiredMoveDir;
+    private Vector3 momentumDir;
+    private float momentumMag;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (!PCAM)
+        {
+            PCAM = GetComponent<PlayerColAnimManager>();
+        }
+        if (!PCAM)
+        {
+            throw new NullReferenceException("Could not get instance of Player Collision Animation Manager!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
