a1f3a451295f155a5f8b5fd9a1aad9a46779fdcb
defaultsaas <thecarldun@gmail.com>
2026-06-09 22:03:27 +0100

    Jump curves do things

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,8 @@ public class PlayerController : MonoBehaviour
     
     private float _momentumMag;
     private float _remainingJumpTime;
+    private float _jumpCurveMaxX = 1.0f;
+    private bool _isFirstJumpUpdate = false;
     
     private InputAction _jumpInputAction;
 
@@ -50,8 +52,9 @@ public class PlayerController : MonoBehaviour
     {
         if (_remainingJumpTime <= 0.0f && _jumpInputAction.WasPerformedThisFrame())
         {
-            Debug.Log("Jump!");
             _remainingJumpTime = JumpTime;
+            _jumpCurveMaxX = JumpCurve.keys[JumpCurve.keys.Length - 1].time;
+            _isFirstJumpUpdate = true;
         }
     }
 
@@ -59,10 +62,46 @@ public class PlayerController : MonoBehaviour
     {
         float dt = Time.fixedDeltaTime;
 
-        if (_remainingJumpTime > 0.0f)
+        if (_remainingJumpTime > 0.0f && !_isFirstJumpUpdate)
         {
-            _rigidbody.AddForce(((Vector3.up * 9.81f) + (Vector3.up * JumpCurve.Evaluate((JumpTime - _remainingJumpTime) / JumpTime) * JumpHeight)) * dt, ForceMode.Impulse);
+            float t = (JumpTime - _remainingJumpTime) / JumpTime;
+            float acceleration = CalcCurveAcceleration(JumpCurve, _jumpCurveMaxX, t, dt / JumpTime);
+            
+            float force = acceleration * JumpHeight;
+            force /= JumpTime * JumpTime;
+            force -= Physics.gravity.y;
+
+            _rigidbody.AddForce(Vector3.up * force, ForceMode.Acceleration);
             _remainingJumpTime -= dt;   
         }
+        else if (_isFirstJumpUpdate)
+        {
+            _isFirstJumpUpdate = false;
+            float firstCurveHeight = JumpCurve.Evaluate(dt / JumpTime) * JumpHeight;
+            float speed = firstCurveHeight / dt;
+            
+            speed -= Physics.gravity.y * dt;
+            _rigidbody.AddForce(Vector3.up * speed, ForceMode.VelocityChange);
+        }
+    }
+
+    public static float CalcCurveAcceleration(AnimationCurve inCurve, float inMaxX, float inT, float inH)
+    {
+        float tPlusH = inT + inH;
+        float tPlus2H = inT + (2.0f * inH);
+
+        // Have to do extra faff if tPlus2H is > 1.0f,
+        // so do below as a backup.
+        if (tPlus2H > inMaxX)
+        {
+            return CalcCurveAcceleration(inCurve, inMaxX, inMaxX - (2.0f * inH), inH);
+        }
+
+        float fOfTPlus2H = inCurve.Evaluate(tPlus2H);
+        float fOfTPlusH = inCurve.Evaluate(tPlusH);
+        float fOfT = inCurve.Evaluate(inT);
+        
+        float result = (fOfTPlus2H - (2.0f * fOfTPlusH) + fOfT) / (inH * inH);
+        return result;
     }
 }
